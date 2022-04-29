47b9c136b50363002bbaed05d30c5347423154e3
Luis fernando chambi orosco <lchambio@unsa.edu.pe>
2022-04-29 15:01:53 -0500

    gui

---
+++ src/defecto/calculator.java
@@ -0,0 +1,7 @@
+package defecto;
+public class calculator{
+    public static void main(String args[]){
+        System.out.print("calculadora ps mi king");
+        System.out.print("que fue mi king nose que son ese add mod xd");
+    }
+}
