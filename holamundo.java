c79147fb5f2beb2479a685d57d38f562658aea5f
Luis fernando chambi orosco <lchambio@unsa.edu.pe>
2022-04-29 10:50:17 -0500

    Probando el Hola Mundo

---
+++ holamundo.java
@@ -0,0 +1,9 @@
+public class holamundo{
+public static void main (String args[]){
+//hola mundo
+System.out.println("¡hola mundo!");
+System.out.println("bienvenidos al curso de programacion web 2");
+
+System.out.println("Ate, luis fernando chambi orosco");
+}
+}
